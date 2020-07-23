ce68369b6e4fa4f8427daba91e93c8cf7a4c4375
Anastasiia Vashchenko <vashchenkonastusia96@gmail.com>
2020-07-23 18:12:14 +0300

    add foulder app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app/App';
 import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(
   // <React.StrictMode>
-    <App a1='5' a2='10'/>,
+    <App />,
   // </React.StrictMode>,
   document.getElementById('root')
 );
